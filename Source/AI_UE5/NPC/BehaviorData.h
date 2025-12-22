355ad73f29729a0d81c4a9edf06b6429ea2a7be8
RaidenTrue <raidmgs@gmail.com>
2025-12-23 01:42:06 +0200

    A.I. DataAsset for Home Location & Search Area

---
+++ Source/AI_UE5/NPC/BehaviorData.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "BehaviorData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AI_UE5_API UBehaviorData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+
+public:
+
+	/* Territory */
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Territory")
+	float WanderRadius = 1500.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Territory")
+	float MaxChaseDistance = 2500.f;
+
+	/* A.I. Perception */
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "A.I. Perception")
+	float LoseInterestDelay = 3.0f;
+
+	/* Seach Behavior */
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Search Behavior")
+	int32 MaxSearchPoints = 5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Search Behavior")
+	float SearchRadius = 800.0f;
+};
